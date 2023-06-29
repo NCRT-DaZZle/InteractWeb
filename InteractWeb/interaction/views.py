25f1fba2f49cb61e57b7c1d6aeffd2fc7fcfe631
Vetle <vetle.h.jensen03@gmail.com>
2023-06-29 18:42:51 +0200

    Added Svelte frontend

--- InteractWeb/interaction/views.py
+++ InteractWeb/interaction/views.py
@@ -1,8 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
-from django.http import HttpResponse
-
-
 def index(request):
-    return HttpResponse("Hello, world. You're at the interaction index.")
+    return render(request, 'frontend/index.html')
---
+++ frontend/src/app.svelte
@@ -0,0 +1,22 @@
+<script>
+    let count = 0;
+  
+    function increment() {
+      count += 1;
+    }
+  </script>
+  
+  <main>
+    <h1>InteractWeb</h1>
+    <p>Count: {count}</p>
+    <button on:click={increment}>Increment</button>
+  </main>
+  
+  <style>
+    main {
+      text-align: center;
+      padding: 2rem;
+      font-family: Arial, sans-serif;
+    }
+  </style>
+  
